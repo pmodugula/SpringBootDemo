8b3cd825b19b611524cc2a511d4a7d38d28cf970
Prasantha <prasanthareddy@gmail.com>
2018-09-02 19:51:43 -0400

    Updated code with Unit testing bot GET and POST services

--- SpringDemoGradleProject/src/main/java/com/example/demo/SpringDemoGradleProjectApplication.java
+++ SpringDemoGradleProject/src/main/java/com/example/demo/SpringDemoGradleProjectApplication.java
@@ -2,11 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringDemoGradleProjectApplication {
+public class SpringDemoGradleProjectApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoGradleProjectApplication.class, args);
-	}
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(SpringDemoGradleProjectApplication.class);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDemoGradleProjectApplication.class, args);
+  }
 }
--- SpringDemoGradleProject/src/main/java/com/example/demo/controller/SumController.java
+++ SpringDemoGradleProject/src/main/java/com/example/demo/controller/SumController.java
@@ -7,24 +7,26 @@ import com.example.demo.model.SumModel;
 
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 @RestController
 
 public class SumController {
 
-    @RequestMapping(value="/math/add" ,method = RequestMethod.GET)
+    @RequestMapping(value="/math/add" ,method = RequestMethod.GET , produces=MediaType.APPLICATION_JSON_VALUE)
     public SumService sumGetController(@RequestParam int param1 ,@RequestParam int param2) {
-        return new SumService(10,20);
+        return new SumService(param1,param2);
     }
  
     
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    @RequestMapping(value = "/math/add", method = RequestMethod.POST , produces=MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public SumService sumPOSTController(@RequestBody SumModel sumModel) {
         return new SumService(sumModel.getParam1(),sumModel.getParam2());
     }
     
-        
+       
+    
 }
 
